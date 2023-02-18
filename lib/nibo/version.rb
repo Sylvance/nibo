6c39a136489c10707b02e853298619bfdb7cdd9d
Sylvance <kerandisylvance@gmail.com>
2023-02-18 10:23:54 +0300

    [nibo] Bundle initial gem files

---
+++ lib/nibo/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Nibo
+  VERSION = "0.1.0"
+end
---
+++ spec/nibo_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Nibo do
+  it "has a version number" do
+    expect(Nibo::VERSION).not_to be nil
+  end
+end
